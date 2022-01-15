f94ad597d22442f4c6b4b63fa1955667fb2963ca
q.com <usa>
2022-01-16 02:14:11 +0800

    final

--- lib/citiy_list.dart
+++ lib/citiy_list.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: prefer_const_constructors
-
 import 'dart:convert';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' as rootBundle;
